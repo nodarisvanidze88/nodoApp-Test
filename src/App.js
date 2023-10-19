e0e504ff9ff06a8f07bebc53ee3e757ea6c4b5ae
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2023-10-19 12:43:09 +0400

    create first items in packing list

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
 export default function App() {
   return (
     <div className="app">
@@ -24,11 +28,15 @@ function Form() {
 }
 function PackingList() {
   return (
-    <div className="list">LIST
-
+    <div className="list">
+      <ul>
+        {initialItems.map(item => <Item item={item} />)}
+      </ul>
     </div>
   )
-
+}
+function Item({ item }) {
+  return <li>{item.description}</li>
 }
 function Stats() {
   return (
